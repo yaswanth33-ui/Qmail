3cc499dab8678822041af613bbdb5f8df0ec5535
yaswanth31-ui <yaswanthreddypanem.ai@gmail.com>
2026-03-02 21:24:35 +0530

    Remove nginx folder from git

--- scripts/smoke_check.py
+++
@@ -1,8 +0,0 @@
-import qmail.api as api
-import time
-print('klen=', len(api._derive_email_key('u','a@x','b@x','s')))
-print('revoked_before=', api._is_token_revoked('faketoken'))
-api._revoke_token('faketoken', ttl_seconds=1)
-print('revoked_after=', api._is_token_revoked('faketoken'))
-time.sleep(1.1)
-print('revoked_expired=', api._is_token_revoked('faketoken'))
